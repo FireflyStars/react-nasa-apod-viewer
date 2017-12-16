98debca5ab618df87d77774223ea6ff45bfbf7e4
velopert <public.velopert@gmail.com>
2017-12-16 09:46:06 +0900

    implemented next/prev feature

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ViewerTemplate from './components/ViewerTemplate';
 import SpaceNavigator from './components/SpaceNavigator';
 import Viewer from './components/Viewer';
-
+import moment from 'moment';
 import * as api from './lib/api';
 
 class App extends Component {
@@ -23,7 +23,7 @@ class App extends Component {
     });
 
     try {
-      const response = await api.getAPOD();
+      const response = await api.getAPOD(date);
       // 비구조화 할당 + 새로운 이름 
       const { date: retrievedDate, url, media_type: mediaType } = response.data;
 
@@ -51,18 +51,32 @@ class App extends Component {
       });
   }
 
+  handlePrev = () => {
+    const { date } = this.state;
+    const prevDate = moment(date).subtract(1, 'days').format('YYYY-MM-DD');
+    console.log(prevDate);
+    this.getAPOD(prevDate);
+  }
+
+  handleNext = () => {
+    const { date, maxDate } = this.state;
+    if(date === maxDate) return;
+
+    const nextDate = moment(date).add(1, 'days').format('YYYY-MM-DD');
+    this.getAPOD(nextDate);
+  }
+
   componentDidMount() {
     this.getAPOD();
   }
-  
 
   render() {
-
     const{ url, mediaType,  loading } = this.state;
+    const { handlePrev, handleNext } = this;
 
     return (
       <ViewerTemplate
-        spaceNavigator={<SpaceNavigator/>}
+        spaceNavigator={<SpaceNavigator onPrev={handlePrev} onNext={handleNext}/>}
         viewer={(
           <Viewer 
             url={url}
